c0f9a91e10fbbec989456f166b5290abd659d415
Edson Andrade <e.andrade666@gmail.com>
2024-11-26 09:16:00 +0100

    primeiros Scripts em python

---
+++ ScriptsPython/00-teste.py
@@ -0,0 +1,6 @@
+Python 3.12.3 (main, Nov  6 2024, 18:32:19) [GCC 13.2.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> valor01=10
+>>> valor02=20
+>>> print(valor01 + valor02)
+30
